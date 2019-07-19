1f3dd712b22f36c9d6599ae061ebcac8d2dc9403
dluha95 <wsedeng@gmail.com>
2019-07-19 18:21:47 +0700

    project pertama

---
+++ src/KoneksiDatabase.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dluha
+ */
+import java.sql.*;
+
+public class KoneksiDatabase {
+ private static Connection koneksi;
+ public static Connection getKoneksi(){
+  if (koneksi == null){
+   try {String url = "jdbc:mysql://localhost:3306/xxx"; // nama databasenya login
+          String user="root"; // username mysql root
+          String pass=""; //passwordnya kosong
+
+ DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+koneksi = DriverManager.getConnection(url , user, pass);
+
+ } 
+   catch(SQLException t){
+     System.out.print("Gagal melakukan koneksi ke database");
+ }
+}
+
+return koneksi;
+}
+ } 
+
